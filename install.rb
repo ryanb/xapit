d8dbae33b5e2dae5bc361ce37e21b96527cd7a5b
Ryan Bates <ryan@railscasts.com>
2009-03-13 08:18:06 -0700

    fixing install script so it opens file properly

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
-File.open("#{Rails.root}/config/initializers/setup_xapit.rb") do |f|
+File.open("#{Rails.root}/config/initializers/setup_xapit.rb", "w") do |f|
   f.write <<-STR
 Xapit::Config.setup(:database_path => "\#{Rails.root}/db/xapiandb")
 STR
