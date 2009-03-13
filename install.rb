871e7d451a7760d30b6387c79188e1a93d41eccf
Ryan Bates <ryan@railscasts.com>
2009-03-13 08:21:07 -0700

    adding uninstall script for removing setup initializer file

--- install.rb
+++ install.rb
@@ -1,3 +1,4 @@
+puts "Adding setup_xapit.rb initializer."
 File.open("#{Rails.root}/config/initializers/setup_xapit.rb", "w") do |f|
   f.write <<-STR
 Xapit::Config.setup(:database_path => "\#{Rails.root}/db/xapiandb")
---
+++ uninstall.rb
@@ -0,0 +1,2 @@
+puts "Removing setup_xapit.rb initializer."
+File.delete("#{Rails.root}/config/initializers/setup_xapit.rb")
