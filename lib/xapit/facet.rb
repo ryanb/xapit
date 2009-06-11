e5b1308c862b49951e6ce59598f0dbc525dd35e6
Ryan Bates <ryan@railscasts.com>
2009-06-11 15:36:04 -0700

    don't fetch record from sql database when listing facet options - closes #17

--- lib/xapit/facet.rb
+++ lib/xapit/facet.rb
@@ -24,18 +24,19 @@ module Xapit
     def options
       matching_identifiers.map do |identifier, count|
         option = FacetOption.find(identifier)
-        option.count = count
-        option.existing_facet_identifiers = @existing_facet_identifiers
-        option
-      end.sort_by(&:name)
+        if option.facet.attribute == @blueprint.attribute
+          option.count = count
+          option.existing_facet_identifiers = @existing_facet_identifiers
+          option
+        end
+      end.compact.sort_by(&:name)
     end
     
     def matching_identifiers
       result = {}
       matches.each do |match|
-        class_name, id = match.document.data.split('-')
-        record = class_name.constantize.find(id)
-        @blueprint.identifiers_for(record).each do |identifier|
+        identifiers = match.document.terms.map(&:term).grep(/^F/).map { |t| t[1..-1] }
+        identifiers.each do |identifier|
           unless existing_facet_identifiers.include? identifier
             result[identifier] ||= 0
             result[identifier] += (match.collapse_count + 1)
