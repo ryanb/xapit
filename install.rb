c3ca40c342662f08e16243a9c1cecdfad62e96b7
Ryan Bates <ryan@railscasts.com>
2009-06-12 15:23:55 -0700

    fixing install script

--- install.rb
+++ install.rb
@@ -1,6 +1,8 @@
+require 'ftools'
+
 source = File.join(File.dirname(__FILE__), '/rails_generators/xapit/templates/setup_xapit.rb')
 destination = "#{Rails.root}/config/initializers/setup_xapit.rb"
 unless File.exist? destination
-  puts "Adding setup_xapit.rb initializer."
-  File.cp(source, destination)
+  puts "Adding config/initializers/setup_xapit.rb"
+  File.copy(source, destination)
 end
