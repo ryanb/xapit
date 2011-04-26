f996360145e19567692b8856e6ae866c21d32de6
Ryan Bates <ryan@railscasts.com>
2011-04-25 17:11:49 -0700

    improving term indexing

--- lib/xapit/server/indexer.rb
+++ lib/xapit/server/indexer.rb
@@ -8,16 +8,48 @@ module Xapit
       def document
         document = Xapian::Document.new
         document.data = "#{@data[:class]}-#{@data[:id]}"
-        if @data[:attributes]
-          @data[:attributes].each do |name, options|
-            if options[:text]
-              options[:value].split.each do |term|
-                document.add_term(term, options[:text][:weight] || 1)
-              end
-            end
-          end
+        weighted_terms.each do |term, weight|
+          document.add_term(term, weight)
+        end
+        terms.each do |term|
+          document.add_term(term, 1)
+        end
+        values.each do |identifier, value|
+          document.add_value(Xapit.value_index(identifier), value)
         end
         document
+      end
+
+      def terms
+        ["C#{@data[:class]}", "Q#{@data[:class]}-#{@data[:id]}"] + field_terms
+      end
+
+      def weighted_terms
+        each_attribute(:text) do |name, value, options|
+          value.split.map do |term|
+            [term, options[:weight] || 1]
+          end
+        end.flatten(1)
+      end
+
+      private
+
+      def field_terms
+        each_attribute(:field) do |name, value, options|
+          "X#{name}-#{value.downcase}"
+        end
+      end
+
+      def each_attribute(type)
+        if @data[:attributes]
+          @data[:attributes].map do |name, options|
+            if options.has_key? type
+              yield(name, options[:value], options[type])
+            end
+          end.compact
+        else
+          []
+        end
       end
     end
   end
--- spec/xapit/server/indexer_spec.rb
+++ spec/xapit/server/indexer_spec.rb
@@ -5,18 +5,33 @@ describe Xapit::Server::Indexer do
     indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "hello world", :text => {}}, :name => {:value => "John", :text => {}}})
     document = indexer.document
     document.should be_kind_of(Xapian::Document)
-    document.terms.map(&:term).sort.should == %w[hello world John].sort
+    document.terms.map(&:term).should include(*%w[hello world John])
   end
 
   it "generates a xapian document with text weight data" do
     indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "hello", :text => {:weight => 3}}})
-    indexer.document.terms.first.wdf.should == 3
+    indexer.document.terms.map(&:wdf).should include(3)
   end
 
   it "adds the id and class to xapian document data" do
-    indexer = Xapit::Server::Indexer.new(:id => "123", :class => "Foo")
+    indexer = Xapit::Server::Indexer.new(:class => "Foo", :id => "123")
     document = indexer.document
     document.should be_kind_of(Xapian::Document)
     document.data.should == "Foo-123"
+  end
+
+  it "includes class/id in terms list" do
+    indexer = Xapit::Server::Indexer.new(:class => "Foo", :id => "123")
+    indexer.terms.should include("CFoo", "QFoo-123")
+  end
+
+  it "includes fields in terms list" do
+    indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "Hello", :field => {}}})
+    indexer.terms.should include("Xgreeting-hello")
+  end
+
+  it "includes test attributes in weighted terms list" do
+    indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "hello world", :text => {}}})
+    indexer.weighted_terms.should include(["hello", 1], ["world", 1])
   end
 end
