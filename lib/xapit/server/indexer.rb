1d7de7dae3f33d96f84b392d662105414074689e
Ryan Bates <ryan@railscasts.com>
2011-04-26 10:49:49 -0700

    indexing values

--- lib/xapit/server/indexer.rb
+++ lib/xapit/server/indexer.rb
@@ -8,35 +8,61 @@ module Xapit
       def document
         document = Xapian::Document.new
         document.data = "#{@data[:class]}-#{@data[:id]}"
-        weighted_terms.each do |term, weight|
+        terms.each do |term, weight|
           document.add_term(term, weight)
         end
-        terms.each do |term|
-          document.add_term(term, 1)
-        end
-        values.each do |identifier, value|
-          document.add_value(Xapit.value_index(identifier), value)
+        values.each do |index, value|
+          document.add_value(index, value)
         end
         document
       end
 
       def terms
-        ["C#{@data[:class]}", "Q#{@data[:class]}-#{@data[:id]}"] + field_terms
+        base_terms + text_terms + field_terms
       end
 
-      def weighted_terms
+      def values
+        values = {}
+        each_value do |type, name, value, options|
+          values[Xapit.value_index(type, name)] = value
+        end
+        values
+      end
+
+      private
+
+      def base_terms
+        [["C#{@data[:class]}", 1], ["Q#{@data[:class]}-#{@data[:id]}", 1]]
+      end
+
+      def text_terms
         each_attribute(:text) do |name, value, options|
-          value.split.map do |term|
+          value.to_s.split.map do |term|
             [term, options[:weight] || 1]
           end
         end.flatten(1)
       end
 
-      private
-
       def field_terms
         each_attribute(:field) do |name, value, options|
-          "X#{name}-#{value.downcase}"
+          ["X#{name}-#{parse_field(value)}", 1]
+        end
+      end
+
+      def parse_field(value)
+        if value.kind_of? Time
+          value.to_i
+        else
+          value.to_s.downcase
+        end
+      end
+
+      def each_value
+        each_attribute(:field) do |name, value, options|
+          yield(:field, name, value, options)
+        end
+        each_attribute(:sortable) do |name, value, options|
+          yield(:sortable, name, value, options)
         end
       end
 
@@ -44,9 +70,13 @@ module Xapit
         if @data[:attributes]
           @data[:attributes].map do |name, options|
             if options.has_key? type
-              yield(name, options[:value], options[type])
+              if options[:value].kind_of? Array
+                options[:value].map { |value| yield(name, value, options[type]) }
+              else
+                [yield(name, options[:value], options[type])]
+              end
             end
-          end.compact
+          end.compact.flatten(1)
         else
           []
         end
--- spec/xapit/server/indexer_spec.rb
+++ spec/xapit/server/indexer_spec.rb
@@ -1,11 +1,12 @@
 require "spec_helper"
 
 describe Xapit::Server::Indexer do
-  it "generates a xapian document with text data" do
-    indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "hello world", :text => {}}, :name => {:value => "John", :text => {}}})
+  it "generates a xapian document with terms and values" do
+    indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "hello world", :text => {}}, :name => {:value => "John", :field => {}}})
     document = indexer.document
     document.should be_kind_of(Xapian::Document)
-    document.terms.map(&:term).should include(*%w[hello world John])
+    document.terms.map(&:term).should include(*%w[hello world])
+    document.values.map(&:value).should == ["John"]
   end
 
   it "generates a xapian document with text weight data" do
@@ -22,16 +23,41 @@ describe Xapit::Server::Indexer do
 
   it "includes class/id in terms list" do
     indexer = Xapit::Server::Indexer.new(:class => "Foo", :id => "123")
-    indexer.terms.should include("CFoo", "QFoo-123")
+    indexer.terms.should include(["CFoo", 1], ["QFoo-123", 1])
   end
 
   it "includes fields in terms list" do
     indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "Hello", :field => {}}})
-    indexer.terms.should include("Xgreeting-hello")
+    indexer.terms.should include(["Xgreeting-hello", 1])
   end
 
-  it "includes test attributes in weighted terms list" do
+  it "indexes array values separately" do
+    indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => ["Hello", "world"], :field => {}}})
+    indexer.terms.should include(["Xgreeting-hello", 1], ["Xgreeting-world", 1])
+  end
+
+  it "includes text attributes in terms list" do
     indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "hello world", :text => {}}})
-    indexer.weighted_terms.should include(["hello", 1], ["world", 1])
+    indexer.terms.should include(["hello", 1], ["world", 1])
+  end
+
+  it "handles odd text attributes correctly" do
+    indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => [6, nil, ""], :text => {}}})
+    indexer.terms.should include(["6", 1])
+    indexer.terms.should_not include(["", 1])
+    indexer.terms.should_not include([nil, 1])
+  end
+
+  it "handles odd field attributes correctly" do
+    # TODO add date handling
+    time = Time.now
+    indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => [6, nil, time], :field => {}}})
+    indexer.terms.should include(["Xgreeting-6", 1], ["Xgreeting-", 1], ["Xgreeting-#{time.to_i}", 1])
+  end
+
+  it "includes field and sortable values" do
+    indexer = Xapit::Server::Indexer.new(:attributes => {:greeting => {:value => "Hello", :field => {}, :sortable => {}}})
+    indexer.values[Xapit.value_index(:field, "greeting")].should == "Hello"
+    indexer.values[Xapit.value_index(:sortable, "greeting")].should == "Hello"
   end
 end
