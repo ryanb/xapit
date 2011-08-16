634bc089d4596034f7f7c810e57a7d441d0564ba
Ryan Bates <ryan@railscasts.com>
2011-08-16 10:12:58 -0700

    improving xapit:index rake task

--- lib/xapit/client/tasks.rb
+++ lib/xapit/client/tasks.rb
@@ -1,14 +1,18 @@
 require "rack"
+require "fileutils"
 
 namespace :xapit do
   desc "Index all models for Xapit search"
   task :index => :environment do
-    models = ActiveRecord::Base.send(:subclasses)
+    raise "No Xapian database specified in config." if Xapit.config[:database_path].blank?
+    FileUtils.rm_rf("tmp/xapit") if File.exist? "tmp/xapit"
+    FileUtils.mv(Xapit.config[:database_path], "tmp/xapit") if File.exist? Xapit.config[:database_path]
+    models = ActiveRecord::Base.subclasses
     Dir[Rails.root.join("app", "models", "**", "*.rb")].each do |file|
-      models << File.basename(file, ".*").classify
+      # I hate to rescue nil, maybe there's a better way to handle unknown constants
+      models << File.basename(file, ".*").classify.constantize rescue nil
     end
-    models.uniq!
-    models.select! { |m| m.respond_to? :xapit_model_adapter }
-    Xapit.index(*models)
+    xapit_models = models.compact.uniq.select { |m| m.respond_to? :xapit_model_adapter }
+    Xapit.index(*xapit_models)
   end
 end
