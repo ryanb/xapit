8d5e9892db703a5b402791c57cbb928cd9ad6221
David A. Cuadrado <krawek@gmail.com>
2011-09-28 12:52:07 -0500

    add mongoid adapter
    Signed-off-by: David A. Cuadrado <krawek@gmail.com>

---
+++ lib/xapit/client/model_adapters/mongoid_adapter.rb
@@ -0,0 +1,27 @@
+module Xapit
+  module Client
+    class MongoidAdapter < AbstractModelAdapter
+      def self.for_class?(model_class)
+        model_class.kind_of?(Mongoid::Document)
+      end
+
+      def setup
+        @model_class.after_create do |member|
+          member.class.xapit_index_builder.add_document(member) if Xapit.config[:enabled]
+        end
+        @model_class.after_update do |member|
+          member.class.xapit_index_builder.update_document(member) if Xapit.config[:enabled]
+        end
+        @model_class.after_destroy do |member|
+          member.class.xapit_index_builder.remove_document(member) if Xapit.config[:enabled]
+        end
+      end
+
+      def index_all
+        @model_class.all.each do |member|
+          member.class.xapit_index_builder.add_document(member)
+        end
+      end
+    end
+  end
+end
